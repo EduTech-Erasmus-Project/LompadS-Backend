03cda1c7032bf6a7965668c1874d13d5df670160
Charly1590 <51939416+Charly1590@users.noreply.github.com>
2022-01-07 23:28:44 -0500

    comit

--- model/Estructuras/LOM/Rights.py
+++ model/Estructuras/LOM/Rights.py
@@ -85,9 +85,9 @@ class Rights:
                 self.value=atributes.get('value')
                 self.description=atributes.get('es')
                 if self.description is None:
-                    self.description=atributes.get('description')
+                    self.description=atributes.get('string')
                     if len(self.description) > 1:
-                        self.description=[atributes.get('description')[1]]
+                        self.description=[atributes.get('string')[1]]
 
             def to_xml(self):
                 return f"""<accessType >
